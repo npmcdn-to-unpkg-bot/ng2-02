69bdf03edf5181f8c2010db631e53419407b0782
remco <remco@RBITDEV01>
2016-06-06 21:55:00 +0200

    Add project files.

---
+++ src/ng2-02/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace ng2_02
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html"; // Put your Angular root page here 
+                    await next();
+                }
+            });
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ src/ng2-02/gulpfile.js
@@ -0,0 +1,54 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    less = require("gulp-less");
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+var paths = {};
+paths.webroot = "wwwroot/";
+paths.npmSrc = "./node_modules/";
+paths.npmLibs = paths.webroot + "lib/npmlibs/";
+
+gulp.task("copy-deps:systemjs", function () {
+    return gulp.src(paths.npmSrc + '/systemjs/dist/**/*.*', { base: paths.npmSrc + '/systemjs/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/systemjs/'));
+});
+
+gulp.task("copy-deps:angular2", function () {
+    return gulp.src(paths.npmSrc + '/angular2/bundles/**/*.js', { base: paths.npmSrc + '/angular2/bundles/' })
+         .pipe(gulp.dest(paths.npmLibs + '/angular2/'));
+});
+
+gulp.task("copy-deps:es6-shim", function () {
+    return gulp.src(paths.npmSrc + '/es6-shim/es6-sh*', { base: paths.npmSrc + '/es6-shim/' })
+         .pipe(gulp.dest(paths.npmLibs + '/es6-shim/'));
+
+});
+gulp.task("copy-deps:es6-promise", function () {
+    return gulp.src(paths.npmSrc + '/es6-promise/dist/**/*.*', { base: paths.npmSrc + '/es6-promise/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/es6-promise/'));
+});
+
+gulp.task("copy-deps:rxjs", function () {
+    return gulp.src(paths.npmSrc + '/rxjs/bundles/*.*', { base: paths.npmSrc + '/rxjs/bundles/' })
+         .pipe(gulp.dest(paths.npmLibs + '/rxjs/'));
+});
+
+gulp.task("copy-assets-less", function () {
+    return gulp.src('assets/styles/*.less')
+      .pipe(less())
+      .pipe(gulp.dest(paths.webroot + '/css'));
+});
+
+gulp.task("copy-assets-scripts", function () {
+    return gulp.src('assets/scripts/*.js')
+      .pipe(gulp.dest(paths.webroot + '/scripts'));
+});
+
+gulp.task("copy-deps", ["copy-deps:rxjs", 'copy-deps:angular2', 'copy-deps:systemjs', 'copy-deps:es6-shim', 'copy-deps:es6-promise', 'copy-assets-less', 'copy-assets-scripts']);
