3c01f86bd2daa5fa60f0fb3d3a6d75a0a1120095
remco <remco@RBITDEV01>
2016-06-13 14:22:13 +0200

    Customised al lot of elemenst, still a few to go

--- src/ng2-02/assets/scripts/morris-data.js
+++ src/ng2-02/assets/scripts/morris-data.js
@@ -1,64 +1,40 @@
 $(function() {
 
-    Morris.Area({
+    Morris.Line({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            day: '2016-06-13',
+            errors: 125,
+            messages: 2389,
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            day: '2016-06-14',
+            errors: 68,
+            messages: 2899,
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            day: '2016-06-15',
+            errors: 298,
+            messages: 1567,
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            day: '2016-06-16',
+            errors: 123,
+            messages: 3778,
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            day: '2016-06-17',
+            errors: 67,
+            messages: 1342,
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            day: '2016-06-18',
+            errors: 140,
+            messages: 2567,
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            day: '2016-06-19',
+            errors: 178,
+            messages: 2780,
         }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
-        hideHover: 'auto',
-        resize: true
+        xkey: 'day',
+        ykeys: ['messages', 'errors'],
+        labels: ['Messages', 'Errors'],
+        lineColors: ['green', 'red'],
     });
 
     Morris.Donut({
@@ -75,43 +51,4 @@ $(function() {
         }],
         resize: true
     });
-
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
-
 });
